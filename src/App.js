596cf0aed8ca8d5e4d7a4f010baba8a514464f61
fanuelstephen <fanuelgobrey@gmail.com>
2023-08-29 19:56:16 +0300

    add overflow hiden

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./component/home/header/Header";
 import HomePages from "./component/pageContent/HomePages";
 import Footer from "./component/home/header/Footer";
-import React, { useEffect } from "react";
+
 
 const App = () => {
-  useEffect(() => {
-    window.scrollTo(0, 500);
-  }, []);
   return (
     <>
       <Router>
         <Header />
         <Switch>
           <Route path="/" exact component={HomePages} />
-          {/* <Route path="/pages" exact component={Pages} /> */}
         </Switch>
         <Footer />
       </Router>
